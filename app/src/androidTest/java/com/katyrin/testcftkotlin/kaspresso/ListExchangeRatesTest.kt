574d3c181b618f5e586bb0cc180ce124ea568d78
Roman <katyrinroman@mail.ru>
2021-05-02 18:14:29 +0700

    fixed notes in tests

--- app/src/androidTest/java/com/katyrin/testcftkotlin/kaspresso/ListExchangeRatesTest.kt
+++ app/src/androidTest/java/com/katyrin/testcftkotlin/kaspresso/ListExchangeRatesTest.kt
@@ -1,16 +1,8 @@
 package com.katyrin.testcftkotlin.kaspresso
 
 
-import android.view.View
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.agoda.kakao.recycler.RecyclerActions
-import com.agoda.kakao.text.KTextView
 import com.katyrin.testcftkotlin.kaspresso.screen.DialogScreen
 import com.katyrin.testcftkotlin.kaspresso.screen.MainScreen
 import com.katyrin.testcftkotlin.view.MainActivity
@@ -28,45 +20,122 @@ class ListExchangeRatesTest: KTestCase() {
     fun checkListExchangeRates() {
 
         run {
-            step("Check currency content") {
-                checkCurrencies(
-                    Currency("Австралийский доллар (36)", "AUD", "1", "58.1546"),
-                    Currency("Азербайджанский манат (944)", "AZN", "1", "44.0524"),
-                    Currency("Армянских драмов (51)", "AMD", "100", "14.3761"),
-                    Currency("Белорусский рубль (933)", "BYN", "1", "29.2398"),
-                    Currency("Болгарский лев (975)", "BGN", "1", "46.295")
-                )
+            step("Check currency AUD") {
+                MainScreen {
+                    currenciesRecyclerView {
+                        childAt<MainScreen.CurrencyItem>(0) {
+                            currencyName {
+                                isDisplayed()
+                                hasText("Австралийский доллар (36)")
+                            }
+                            currencyCharCode {
+                                isDisplayed()
+                                hasText("AUD")
+                            }
+                            currencyNominal {
+                                isDisplayed()
+                                hasText("1")
+                            }
+                            currencyValue {
+                                isDisplayed()
+                                hasText("58.1546")
+                            }
+                        }
+                    }
+                }
             }
-        }
-    }
-
-    class Currency(
-        val currencyName: String,
-        val currencyCharCode: String,
-        val currencyNominal: String,
-        val currencyValue: String,
-    )
-
-    private fun checkCurrencies(vararg currencies: Currency) {
-        currencies.forEachIndexed { index, currency ->
-            MainScreen {
-                currenciesRecyclerView {
-                    childAt<MainScreen.CurrencyItem>(index) {
-                        currencyName {
-                            isDisplayed()
-                            hasText(currency.currencyName)
+            step("Check currency AZN") {
+                MainScreen {
+                    currenciesRecyclerView {
+                        childAt<MainScreen.CurrencyItem>(1) {
+                            currencyName {
+                                isDisplayed()
+                                hasText("Азербайджанский манат (944)")
+                            }
+                            currencyCharCode {
+                                isDisplayed()
+                                hasText("AZN")
+                            }
+                            currencyNominal {
+                                isDisplayed()
+                                hasText("1")
+                            }
+                            currencyValue {
+                                isDisplayed()
+                                hasText("44.0524")
+                            }
                         }
-                        currencyCharCode {
-                            isDisplayed()
-                            hasText(currency.currencyCharCode)
+                    }
+                }
+            }
+            step("Check currency AMD") {
+                MainScreen {
+                    currenciesRecyclerView {
+                        childAt<MainScreen.CurrencyItem>(2) {
+                            currencyName {
+                                isDisplayed()
+                                hasText("Армянских драмов (51)")
+                            }
+                            currencyCharCode {
+                                isDisplayed()
+                                hasText("AMD")
+                            }
+                            currencyNominal {
+                                isDisplayed()
+                                hasText("100")
+                            }
+                            currencyValue {
+                                isDisplayed()
+                                hasText("14.3761")
+                            }
                         }
-                        currencyNominal {
-                            isDisplayed()
-                            hasText(currency.currencyNominal)
+                    }
+                }
+            }
+            step("Check currency BYN") {
+                MainScreen {
+                    currenciesRecyclerView {
+                        childAt<MainScreen.CurrencyItem>(3) {
+                            currencyName {
+                                isDisplayed()
+                                hasText("Белорусский рубль (933)")
+                            }
+                            currencyCharCode {
+                                isDisplayed()
+                                hasText("BYN")
+                            }
+                            currencyNominal {
+                                isDisplayed()
+                                hasText("1")
+                            }
+                            currencyValue {
+                                isDisplayed()
+                                hasText("29.2398")
+                            }
                         }
-                        currencyValue {
-                            isDisplayed()
-                            hasText(currency.currencyValue)
+                    }
+                }
+            }
+            step("Check currency BGN") {
+                MainScreen {
+                    currenciesRecyclerView {
+                        childAt<MainScreen.CurrencyItem>(4) {
+                            currencyName {
+                                isDisplayed()
+                                hasText("Болгарский лев (975)")
+                            }
+                            currencyCharCode {
+                                isDisplayed()
+                                hasText("BGN")
+                            }
+                            currencyNominal {
+                                isDisplayed()
+                                hasText("1")
+                            }
+                            currencyValue {
+                                isDisplayed()
+                                hasText("46.295")
+                            }
                         }
                     }
                 }
@@ -76,31 +145,13 @@ class ListExchangeRatesTest: KTestCase() {
 
     @Test
     fun checkConversion() {
-        var currencyUSD = 0.0
-        var nominal = 0
-        before {
-
-        }
         run {
-            step("Scroll to USD item") {
-                MainScreen {
-                    currenciesRecyclerView {
-                        scrollToPosition(10)
-                    }
-                }
-            }
             step("Click USD") {
                 MainScreen {
                     currenciesRecyclerView {
                         childAt<MainScreen.CurrencyItem>(10) {
                             currencyCharCode {
                                 hasText("USD")
-                            }
-                            currencyValue {
-                                currencyUSD = getText().toDouble()
-                            }
-                            currencyNominal {
-                                nominal = getText().toInt()
                             }
                             click()
                         }
@@ -117,52 +168,10 @@ class ListExchangeRatesTest: KTestCase() {
                         click()
                     }
                     result {
-                        hasText(countResult1000USD(nominal, currencyUSD))
+                        hasText("13.36 USD")
                     }
                 }
             }
         }
-    }
-
-    private fun countResult1000USD(nominal: Int, currencyUSD: Double): String {
-        val result = 1000 * nominal / currencyUSD
-        return String.format("%.2f", result) + " USD"
-    }
-
-
-    private fun RecyclerActions.scrollToPosition(position: Int) {
-        view.perform(object : ViewAction {
-            override fun getDescription() = "Scroll RecyclerView to $position"
-
-            override fun getConstraints() = ViewMatchers.isAssignableFrom(RecyclerView::class.java)
-
-            override fun perform(controller: UiController, view: View) {
-                if (view is RecyclerView) {
-                    if (position < view.adapter!!.itemCount) {
-                        view.scrollToPosition(position)
-                        controller.loopMainThreadUntilIdle()
-                        val lastView = view.findViewHolderForLayoutPosition(position)!!.itemView
-                        view.scrollBy(0, lastView.height)
-                        controller.loopMainThreadUntilIdle()
-                    } else {
-                        throw IndexOutOfBoundsException("$position position is not exist")
-                    }
-                }
-            }
-        })
-    }
-
-    private fun KTextView.getText() : String {
-        var text = ""
-        view.perform(object : ViewAction {
-            override fun getConstraints() = ViewMatchers.isAssignableFrom(TextView::class.java)
-
-            override fun getDescription(): String = "Get text"
-
-            override fun perform(uiController: UiController?, view: View?) {
-                text = (view as TextView).text.toString()
-            }
-        })
-        return text
     }
 }
